036ce7ec972d84c58b775387560b60fa08a60d59
Kungcc <chienchang0822@gmail.com>
2021-08-17 09:10:21 +0800

    keycloak

--- src/app.ts
+++ src/app.ts
@@ -11,9 +11,14 @@ import httpStatus from "http-status";
 import dbConfig from './config/maria';
 import {httpRequestTimer, register} from "./middlewares/PromMiddleware";
 import httpContext from "express-http-context";
+import { auth } from 'express-openid-connect';
+import jose from 'jose';
+import session from 'express-session';
+import createMemoryStore from 'memorystore';
+
 
 const app = express();
-
+app.set('trust proxy', true);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +29,38 @@ app.use(httpContext.middleware);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const MemoryStore = createMemoryStore(session);
+
+app.use(
+  auth({
+    issuerBaseURL: 'http://10.36.1.125:8080/auth/realms/master',
+    baseURL: 'http://localhost:3000',
+    clientID: 'node-test',
+    secret: '32364076-98f7-49b4-b759-83c5b62f885a',
+    clientSecret: '32364076-98f7-49b4-b759-83c5b62f885a',
+    auth0Logout: true,
+    authorizationParams: {
+      response_type: 'code'
+    },
+    session: {
+      store: new MemoryStore({
+        checkPeriod: 86400000,
+      }) as any,
+      cookie: {
+        domain: 'localhost'
+      }
+    },
+    afterCallback: (req, res, session) => {
+      const claims = jose.JWT.decode(session.id_token); 
+      console.log(claims);
+      // if (claims.org_id !== 'Required Organization') {
+      //   throw new Error('User is not a part of the Required Organization');
+      // }
+      return session;
+    }
+  })
+);
 
 const startRouter = express.Router().get('/', function(req, res) {
     res.status(httpStatus.OK).json({ message: 'Hello nodejs restful API' });   
