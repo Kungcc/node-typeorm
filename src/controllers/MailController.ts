1ab37aad5d1640fdd43502e98bd969ad43dd3c97
Kung <chienchang0822@gmail.com>
2021-09-04 15:29:34 +0800

    Update MailController.ts

--- src/controllers/MailController.ts
+++ src/controllers/MailController.ts
@@ -4,16 +4,6 @@ import nodemailer from "nodemailer";
 export default class MailController {
 
     async sendMail(request: Request, response: Response, next: NextFunction) {
-        // var transporter = nodemailer.createTransport({
-        //     host: 'smtp.gmail.com',
-        //     port: 465,
-        //     secure: true,
-        //     socketTimeout: 10000,
-        //     auth: {
-        //         user: 'chienchang0822@gmail.com',
-        //         pass: 'ciombuaulgogqqgc',
-        //     },
-        // });
 
         var transporter = nodemailer.createTransport({
             host: 'smtp1.liontravel.com',
